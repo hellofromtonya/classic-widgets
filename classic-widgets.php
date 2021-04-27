6ac951d12ec764456003f25ee319a9a3ed914ac0
hellofromtonya <hello@hellofromtonya.com>
2021-04-27 13:55:51 -0500

    Adds wp_ filter to cover both Gutenberg plugin and when merged into core.

--- classic-widgets.php
+++ classic-widgets.php
@@ -27,5 +27,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 	die( 'Invalid request.' );
 }
 
-// Disables the block editor from managing widgets.
+// Disables the block editor from managing widgets in the Gutenberg plugin.
 add_filter( 'gutenberg_use_widgets_block_editor', '__return_false' );
+// Disables the block editor from managing widgets.
+add_filter( 'wp_use_widgets_block_editor', '__return_false' );
